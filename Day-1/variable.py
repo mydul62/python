b579ca0f6432ce047d9f256b0bc23f73fa7a2e9a
unknown <mydulcse62.niter@gmail.com>
2025-07-08 23:19:00 +0600

    variable done

--- Day-1/variable.py
+++ Day-1/variable.py
@@ -2,3 +2,18 @@ age = 10
 print(age) 
 
 # variable e data type dite hoy na 
+
+print(type(age))
+
+name = 'mahim'
+
+print(type(name))
+
+# ctrl + / single line of comment
+# alt + shift + a : multi line comment
+
+
+print("mahim" + " " + " islam")
+text = f"mahim islam age: {10}"
+print(text)
+
