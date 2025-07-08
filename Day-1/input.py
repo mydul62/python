e7ae8d75e00c3ac81e00dccf2cec93c3f9925b88
unknown <mydulcse62.niter@gmail.com>
2025-07-08 23:32:37 +0600

    input done

--- Day-1/input.py
+++ Day-1/input.py
@@ -2,3 +2,11 @@ print("now i need money")
 
 x=input("Give me some money")
 print(f"Your money is : {x}")
+
+# type conversion
+
+frist_money = input("give me some money")
+second_money = input("give me some money again ")
+first_money_int = int(frist_money)
+second_money_int = int(second_money)
+print(first_money_int+second_money_int)
