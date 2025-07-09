b75bf11fb9dbc95fc5ccdd6a61cec3fadc27f911
unknown <mydulcse62.niter@gmail.com>
2025-07-09 10:57:40 +0600

    while

--- Day-1/conditionals.py
+++ Day-1/conditionals.py
@@ -2,9 +2,17 @@
 
 
 
-a = 3
+a = 5
+boss = False
+
+# if a>5:
+#  print("a is greter than 5")
+# else:
+#   print("a is not greter than 5")
 
 if a>5:
  print("a is greter than 5")
-else:
-  print("a is not greter than 5")
+elif a>3:
+ print('a is greter than 3')
+else: 
+ print( ' a is less than 3 or 5')
